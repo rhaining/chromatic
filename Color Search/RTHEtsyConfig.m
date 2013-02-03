960de87274b85e4b32fee41b303ffe853a80a0fd
Rob Haining <rob@tolar.ws>
2013-02-03 09:58:14 -0500

    config update

--- Color Search/RTHEtsyConfig.m
+++ Color Search/RTHEtsyConfig.m
@@ -18,3 +18,22 @@
 }
 
 @end
+
+
+
+/**
+
+add a config.plist file with the following format:
+ 
+ <?xml version="1.0" encoding="UTF-8"?>
+ <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+ <plist version="1.0">
+ <dict>
+ <key>etsy_api_key</key>
+ <string>PUT_ETSY_API_KEY_HERE</string>
+ </dict>
+ </plist>
+
+
+
+*/
